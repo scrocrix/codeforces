c9b16e959d45d03bedc7a8dc13f2c97315e2505e
João Paulo Vieira da Silva <scrocrix@gmail.com>
2021-03-12 08:39:23 +0400

    Implement 141a problemset

---
+++ 800/141a_amusing_joke.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+#include "algorithm"
+
+using namespace std;
+
+int main() {
+    string a, b, c, s;
+    cin >> a >> b >> c;
+    s = a + b;
+    sort(s.begin(), s.end());
+    sort(c.begin(), c.end());
+    if (s == c) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
